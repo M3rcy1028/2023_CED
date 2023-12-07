faf4c4adf2e5d1b58200f3183225db1702542d7c
Mercy_:3 <118319485+M3rcy1028@users.noreply.github.com>
2023-12-07 16:14:33 +0900

    Delete 2_2022202075_Assignment_02 directory

--- 2_2022202075_Assignment_02/ha.v
+++
@@ -1,8 +0,0 @@
-//Half Adder
-module ha(s, co, a, b);
-	input a, b;		//input data a and b
-	output s, co;	//sum, carry out
-	
-	_and2 CO(co, a, b);	//CO = a&b
-	_xor2	S(s, a, b);		//S = a^b
-endmodule
